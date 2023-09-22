272c6a2aa42ec60b01f5944a2d462784e55cb670
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-09-22 15:28:09 +0300

    Updated the cors origin list

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -45,9 +45,12 @@ MIDDLEWARE = [
 ]
 
 
-#CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    
+]
 
-CORS_ALLOW_ALL_ORIGINS = True  
+
     
 
 
