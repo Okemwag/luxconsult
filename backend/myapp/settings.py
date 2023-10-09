8b3fce027f5ecb2f2915a8895dd389f7599ddc1d
Gabriel Okemwa <gabrielokemwa83@gmail.com>
2023-10-09 17:29:25 +0300

    Initial Commit

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:3001",
+    "https://luxconsult-o5h4.vercel.app/",
     
 ]
 
@@ -151,7 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
