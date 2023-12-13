faab1a2920c7a7bcea2e85cb10a01ead10adf13f
WaruiAlfred <alfredwarui17@gmail.com>
2023-12-13 23:11:09 +0300

    configure to use rtk hook

--- frontend/src/components/Sales.jsx
+++ frontend/src/components/Sales.jsx
@@ -1,12 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import '../App.css';
-
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import { useGetForSaleQuery } from "../redux/services/properties";
 
 function Sales() {
-    const [propertyData, setPropertyData] = useState([]);
+  const { data } = useGetForSaleQuery();
+  const [propertyData, setPropertyData] = useState([]);
 
+  useEffect(() => {
+    if (data) {
+      // Limit the photos to a maximum of four
+      const limitedData = data.results.slice(0, 4);
+      setPropertyData(limitedData);
+    }
+  }, [data]);
+
+  /*
     useEffect(() => {
         // Fetch data from your Django API using Axios
         axios.get('http://127.0.0.1:8000/api/v1/core/for-sale/')
@@ -19,43 +28,43 @@ function Sales() {
                 console.error('Error fetching data:', error);
             });
     }, []);
+*/
 
-    
-    return (
-        
-        <div className="container grid-3">
-            <div className=" sales-container x-start">
-                <h1>LATEST LAUNCHES</h1>
-                     <p>Luxconsult offers investment-grade developments all over the City. The developments we offer have been conceptualized by our development team to be high quality assets using a data-driven approach in planning and design, that gears property for high rental returns and an escalated rate of capital growth.
-                      </p>
-                <Link to="/sales" className="btn btn-primary">
-                     View All
-                    </Link>
-            </div>
-            <div className="container grid-3">
-                {propertyData.map((property) => (
-                    <div key={property.id} className="img-container">
-                        <Link to={`property/${property.id}`}>
-                            <img src={property.main_photo} alt={property.title} className="property-image" />
-                            <h2 className="property-title">{property.title}</h2>
-                        </Link>
-                        <p className="property-description">{property.description}</p>
-                        <p className="property-details">
-                            {property.bedrooms} Bedrooms | Ksh:{property.price}
-                        </p>
-                    </div>
-                    
-    ))}
-            </div>
-            </div>            
-    )
-    
+  return (
+    <div className="container grid-3">
+      <div className=" sales-container x-start">
+        <h1>LATEST LAUNCHES</h1>
+        <p>
+          Luxconsult offers investment-grade developments all over the City. The
+          developments we offer have been conceptualized by our development team
+          to be high quality assets using a data-driven approach in planning and
+          design, that gears property for high rental returns and an escalated
+          rate of capital growth.
+        </p>
+        <Link to="/sales" className="btn btn-primary">
+          View All
+        </Link>
+      </div>
+      <div className="container grid-3">
+        {propertyData.map((property) => (
+          <div key={property.id} className="img-container">
+            <Link to={`property/${property.id}`}>
+              <img
+                src={property.main_photo}
+                alt={property.title}
+                className="property-image"
+              />
+              <h2 className="property-title">{property.title}</h2>
+            </Link>
+            <p className="property-description">{property.description}</p>
+            <p className="property-details">
+              {property.bedrooms} Bedrooms | Ksh:{property.price}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-     
-        
-      
-    
 
-    
 export default Sales;
-
--- frontend/src/pages/SalesPage.jsx
+++ frontend/src/pages/SalesPage.jsx
@@ -1,11 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios'; 
-import '../App.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+import { useGetForSaleQuery } from "../redux/services/properties";
 
 function Sales() {
+  const { data } = useGetForSaleQuery();
   const [propertyData, setPropertyData] = useState([]);
 
+  useEffect(() => {
+    if (data) {
+      // Limit the photos to a maximum of four
+      const limitedData = data.results.slice(0, 4);
+      setPropertyData(limitedData);
+    }
+  }, [data]);
+
+  /*
   useEffect(() => {
     // Fetch all data from your Django API using Axios
     axios.get('http://127.0.0.1:8000/api/v1/core/for-sale/')
@@ -16,14 +26,23 @@ function Sales() {
         console.error('Error fetching data:', error);
       });
   }, []);
+  */
 
   return (
     <div>
-      <h1 className='headerWrapper'>PROPERTIES FOR SALE</h1>
+      <h1 className="headerWrapper">PROPERTIES FOR SALE</h1>
       <div className="container x-start">
         {propertyData.map((property) => (
-          <Link to={`/property/${property.id}`} key={property.id} className="img-container">
-            <img src={property.main_photo} alt={property.title} className="property-image" />
+          <Link
+            to={`/property/${property.id}`}
+            key={property.id}
+            className="img-container"
+          >
+            <img
+              src={property.main_photo}
+              alt={property.title}
+              className="property-image"
+            />
             <div className="property-info">
               <h2 className="property-title">{property.title}</h2>
               <p className="property-description">{property.description}</p>
@@ -34,7 +53,6 @@ function Sales() {
           </Link>
         ))}
       </div>
-      
     </div>
   );
 }
