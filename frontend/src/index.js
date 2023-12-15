94cc5e7e47b8568ddcf2a844286353998fcfab5e
WaruiAlfred <alfredwarui17@gmail.com>
2023-12-15 12:33:38 +0300

    install and configure tailwind css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'; // Import ReactDOM correctly
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client"; // Import ReactDOM correctly
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>
 );
-
-
-
