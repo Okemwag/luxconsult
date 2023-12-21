a80d203b183641c534d652ad2f5016b7005ce952
Okemwag <gabrielokemwa83@gmail.com>
2023-12-21 14:25:39 +0300

    Update database configuration

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -117,16 +117,16 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 """
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -137,7 +137,7 @@ DATABASES = {
         'PORT': os.getenv("PGPORT"),
     }
 }
-"""
+
 
 
 # Password validation
