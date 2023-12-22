0cd6e5955bca86ae749323c3c7e9be9c8ab1fe36
Okemwag <gabrielokemwa83@gmail.com>
2023-12-22 16:27:28 +0300

    Change database engine to SQLite3

--- backend/myapp/settings.py
+++ backend/myapp/settings.py
@@ -110,14 +110,11 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ["PGDATABASE"],
-        'USER': os.environ["PGUSER"],
-        'PASSWORD': os.environ["PGPASSWORD"],
-        'HOST': os.environ["PGHOST"],
-        'PORT': os.environ["PGPORT"],
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 """
 DATABASES = {
