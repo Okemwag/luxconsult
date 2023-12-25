5767ccf65ae3c8ed51492683131cf03d930c9847
Okemwag <gabrielokemwa83@gmail.com>
2023-12-25 06:55:40 +0300

    Remove unused files and update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Footer from "../src/components/Footer";
 import Invest from "../src/components/Invest";
 import Navbar from "../src/components/Navbar";
-import PropertyDetails from "../src/components/PropertyDetails";
+//import PropertyDetails from "../src/components/PropertyDetails";
 import HomePage from "../src/pages/HomePage";
-import RentalsPage from "../src/pages/RentalsPage";
 import SalesPage from "../src/pages/SalesPage";
 import Virtualtours from "../src/pages/Virtualtours";
+import RentalsPage from "../src/pages/rentalsPage";
 import "./App.css";
 import store from "./redux/store";
+import PropertyCard from "./components/PropertyCard";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -34,7 +35,7 @@ function App() {
           <Route path="/tours" element={<Virtualtours />} />
           <Route path="/invest" element={<Invest />} />
           <Route path="/footer" element={<Footer />} />
-          <Route path="/property/:id" element={<PropertyDetails />} />
+          <Route path="/property/:id" element={<PropertyCard />} />
         </Routes>
         <Footer />
       </div>
