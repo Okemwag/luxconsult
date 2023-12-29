84e50397b381dbfd2907387e750888753176aa62
Okemwag <gabrielokemwa83@gmail.com>
2023-12-29 18:46:46 +0300

    Delete unused image files and update API base URL

--- frontend/src/redux/services/properties.js
+++ frontend/src/redux/services/properties.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const propertiesApi = createApi({
   reducerPath: "propertiesApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://127.0.0.1:8000/api/v1/core/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://luxconsult-production.up.railway.app/api/v1/core/" }),
   endpoints: (builder) => ({
     getForRent: builder.query({
       query: () => `for-rent/`,
