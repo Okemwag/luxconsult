5a7fb5e084e3ddd0fd5888b24135146eaac3ca3f
Okemwag <gabrielokemwa83@gmail.com>
2024-04-24 13:11:48 +0300

    Updated the url photos

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -22,6 +22,41 @@ class PropertySerializer(serializers.ModelSerializer):
         base_url = 'https://luxadmin.live'
         return base_url + photo_url
         
+    
+    def get_photo1(self, property):
+        
+        photo_url = property.photo1.url
+        # base_url = 'http://102.133.146.44:80'
+        base_url = 'https://luxadmin.live'
+        return base_url + photo_url
+    
+    def get_photo2(self, property):
+        
+        photo_url = property.photo2.url
+        # base_url = 'http://102.133.146.44:80'
+        base_url = 'https://luxadmin.live'
+        return base_url + photo_url
+
+    def get_photo3(self, property):
+        
+        photo_url = property.main_photo3.url
+        # base_url = 'http://102.133.146.44:80'
+        base_url = 'https://luxadmin.live'
+        return base_url + photo_url
+    
+    def get_photo4(self, property):
+        
+        photo_url = property.photo4.url
+        # base_url = 'http://102.133.146.44:80'
+        base_url = 'https://luxadmin.live'
+        return base_url + photo_url
+    
+    def get_photo5(self, property):
+        
+        photo_url = property.photo5.url
+        # base_url = 'http://102.133.146.44:80'
+        base_url = 'https://luxadmin.live'
+        return base_url + photo_url
         
         
     
